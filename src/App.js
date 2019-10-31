728f5fe983832c20ab29407caa6db25692a7cee1
Nargiza O <nargiza2k@gmail.com>
2019-10-31 18:58:54 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TodoItem from './TodoItem'
+import './style.css';
+
+function App() {
+  return (
+    <div className="todo-list">
+     <TodoItem/>
+     <TodoItem/>
+     <TodoItem/>
+     <TodoItem/>
+    </div>
+  );
+}
+
+export default App;
