ae3f332ee4704fac134a99cbf6ac3eada5403db3
Nargiza O <nargiza2k@gmail.com>
2019-11-01 18:20:47 -0500

    changed functionality to be dynamic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import TodoItem from './TodoItem'
 import './style.css';
+import todosData from './todosData';
 
 function App() {
+  const todoItems = todosData.map(item => <TodoItem key = {item.id} item={item}/>)
   return (
     <div className="todo-list">
-     <TodoItem/>
-     <TodoItem/>
-     <TodoItem/>
-     <TodoItem/>
+     {todoItems}
     </div>
   );
 }
