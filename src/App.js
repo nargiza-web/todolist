c2c6774857a97f88188fbac5013e39505d065ded
Nargiza O <nargiza2k@gmail.com>
2019-11-02 17:40:42 -0500

    onchange goes and maps to id

--- src/App.js
+++ src/App.js
@@ -3,13 +3,38 @@ import TodoItem from './TodoItem'
 import './style.css';
 import todosData from './todosData';
 
-function App() {
-  const todoItems = todosData.map(item => <TodoItem key = {item.id} item={item}/>)
-  return (
-    <div className="todo-list">
-     {todoItems}
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      todos: todosData
+    }
+    
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(id){
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id){
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
+  }
+  
+  render(){
+    const todoItems = this.state.todos.map(item => <TodoItem key = {item.id} item = {item} handleChange = {this.handleChange}/>)
+    
+    return(
+      <div className = "todo-list">
+         {todoItems}
+      </div>
+    )
+  }
+  
 }
-
 export default App;
