e130bf0156e1c00ff7a2b519caef317d9b6fc250
Nargiza O <nargiza2k@gmail.com>
2019-11-03 13:44:42 -0600

    finalizing my todo list added some conditional styling

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './style.css'
 
 function TodoItem(props){
+    const completedStyle ={
+        textDecoration: "line-through",
+        color: "#cdcdcd",
+        fontStyle: "italic"
+    }
     return (
         <div className="todo-item">
             <input 
                 type="checkbox" 
                 checked = {props.item.completed}
                 onChange = {() => props.handleChange(props.item.id)}/> 
-            <p>{props.item.text}</p>
+            <p style ={props.item.completed ? completedStyle: null}>{props.item.text}</p>
         </div>
     )
 }
