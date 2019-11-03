5bead6fcdeea7ccf2467f5761f6e63a16e8387ff
Nargiza O <nargiza2k@gmail.com>
2019-11-03 11:01:54 -0600

    now returning a new object instead of updating

--- src/App.js
+++ src/App.js
@@ -16,7 +16,11 @@ class App extends React.Component {
     this.setState(prevState => {
       const updatedTodos = prevState.todos.map(todo => {
         if (todo.id === id){
-          todo.completed = !todo.completed
+          // todo.completed = !todo.completed (don't want to update the same instead return a new obj)
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
         }
         return todo
       })
